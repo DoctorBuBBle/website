370fb41ae18d676b8d53c65cc96d80f0f2ad02e5
Sebastian Paas <s-paas@t-online.de>
2020-12-07 01:47:37 -0800

    replaceAll with replaced with replace and regex

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -302,7 +302,7 @@ const AboutSection = ({ title, image, text = "" }) => {
           {/*<img className="about-me-image" src={getImageSrc(image)} alt="Me" />*/}
           <MarkdownAsHTML
             className="about-me-text"
-            markdown={text.replaceAll("[age]", getAge())}
+            markdown={text.replace(/\[age\]/g, getAge())}
           ></MarkdownAsHTML>
         </div>
       </div>
